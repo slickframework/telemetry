d5023e1ebd3a1ad333b268a43edbea90ba627b5b
Filipe Silva <silvam.filipe@gmail.com>
2022-04-06 18:21:25 +0000

    Adds support for PHP 8+

--- spec/TelemetryClient/PsrLoggerClientSpec.php
+++ spec/TelemetryClient/PsrLoggerClientSpec.php
@@ -239,7 +239,7 @@ class EventWasUsed extends AbstractEvent implements SlickEvent, \JsonSerializabl
         parent::__construct();
     }
 
-    public function jsonSerialize()
+    public function jsonSerialize(): string
     {
         return 'it runs';
     }
